6a7c12131d7432020f75b176c180c99758985068
Shawn Dhave <shawnoxy@gmail.com>
2020-02-13 11:14:24 +0200

    Update port

--- app.js
+++ app.js
@@ -21,10 +21,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 let port = process.env.PORT;
+
 if (port == null || port == "") {
   port = 3000;
 }
-app.listen(port);
 
 app.listen(port, function() {
   console.log("Server started successfully");
